5f26a873048519e0867998e797033d6494505d46
choeqq <k.chola@op.pl>
2022-09-13 00:01:52 +0200

    server running

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,18 +2,23 @@ import "reflect-metadata";
 import { createServer } from "./utils/createServer";
 
 async function main() {
-  // start server
   const { app, server } = await createServer();
 
   app.get("/healthcheck", async () => "OK");
 
   await server.start();
 
+  app.register(
+    server.createHandler({
+      cors: false,
+    })
+  );
+
   await app.listen({
     port: 4000,
   });
 
-  console.log(`Server ready at http://localhostL400${server.graphqlPath}`);
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
 main();
